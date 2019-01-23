1985d2aec6528c18b85d5d2db5cc40d2a9aedee5
Ian "The_Cre8r" Harshbarger <TheCre8r@users.noreply.github.com>
2019-01-22 21:11:10 -0500

    Update WME-Toolbox-Hider.user.js

--- WME-Toolbox-Hider.user.js
+++ WME-Toolbox-Hider.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         WME Toolbox Hider
 // @namespace    https://github.com/thecre8r/
-// @version      2019.01.22.03
+// @version      2019.01.22.04
 // @description  Hides the toolbox when hovering over toolbar buttons for those of us that like the toolbox on top
 // @include      https://www.waze.com/editor*
 // @include      https://www.waze.com/*/editor*
@@ -22,7 +22,8 @@
     }
     function MO() {
         log('MO Ran');
-        let TBZ = $('#WMETB_NavBar').css('z-index')
+        let TBZ = $('#WMETB_NavBar').css('z-index');
+        $('.save-popover-container').css('z-index',TBZ+1)
         var targetNode = document.getElementById('edit-buttons');
         var config = { attributes: true, childList: true, subtree: true };
         var callback = function(mutationsList, observer) {
@@ -35,8 +36,7 @@
                         $('#WMETB_NavBar').css('opacity','1');
                         $('#WMETB_NavBar').css('z-index',TBZ);
                     }
-                } else if (mutation.target.id.includes("tooltip")) { //For Waze Edit Count Monitor
-                    log(mutation);
+                } else if (mutation.target.classList[0] == 'tooltip' && mutation.target.id.includes("tooltip")) { //For Waze Edit Count Monitor
                     if(mutation.target.classList[3] == 'in'){
                         $('#WMETB_NavBar').css('opacity','0');
                         $('#WMETB_NavBar').css('z-index','0');
@@ -49,6 +49,28 @@
         };
         var observer = new MutationObserver(callback);
         observer.observe(targetNode, config);
+
+        var targetNode2 = document.getElementById('toolbar');
+        var config2 = { attributes: true, childList: true, subtree: true, CharacterData: true };
+        var callback2 = function(mutationsList, observer) {
+            for(var mutation of mutationsList) {
+                if (mutation.target.classList[0] == 'changes-log-region') {
+                    //log(mutation);
+                    debugger;
+                    if (mutation.addedNodes != undefined && mutation.addedNodes.length > 0 && mutation.addedNodes[0].classList[0] == 'changes-log-container')
+                    {
+                        $('#WMETB_NavBar').css('opacity','0');
+                        $('#WMETB_NavBar').css('z-index','0');
+
+                    } else{
+                        $('#WMETB_NavBar').css('opacity','1');
+                        $('#WMETB_NavBar').css('z-index',TBZ);
+                    }
+                }
+            }
+        };
+        var observer2 = new MutationObserver(callback2);
+        observer2.observe(targetNode2, config2);
     }
     function bootstrap(tries = 1) {
         log('bootstrap attempt '+ tries);
