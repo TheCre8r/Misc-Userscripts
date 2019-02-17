fb364186355208bd72b9355c66af97dc96395230
Ian "The_Cre8r" Harshbarger <TheCre8r@users.noreply.github.com>
2019-02-16 19:08:48 -0500

    Create  Waze-Tolls-UI-Fixer.user.js

---
+++ Waze-Tolls-UI-Fixer.user.js
@@ -0,0 +1,57 @@
+// ==UserScript==
+// @name         Waze Tolls UI Fixer
+// @namespace    https://github.com/thecre8r/
+// @version      2019.02.16.00
+// @description  Presses Login when you hit enter on the password field and automatically presses continue.
+// @match        http://waze-tolls-management.appspot.com/welcome.html
+// @author       The_Cre8r
+// @license      GPLv3
+// @grant        none
+// ==/UserScript==
+
+/* global $ */
+
+(function() {
+    function log(msg) {
+        console.log('Waze Tolls UI Fixer: ', msg);
+    }
+    function Enter() {
+        log('Enter Ran');
+        var input = document.getElementById("password");
+        input.addEventListener("keyup", function(event) {
+            if (event.keyCode === 13) {
+                event.preventDefault();
+                document.querySelector('#loginDiv > table > tbody > tr:nth-child(3) > td:nth-child(2) > input[type="button"]:nth-child(1)').click();
+            }
+        });
+    }
+    function MO() {
+        log('MO Ran');
+        let temp;
+        var targetNode = document.getElementById('cont');
+        var config = { attributes: true, childList: true, subtree: true };
+        var callback = function(mutationsList, observer) {
+            for(var mutation of mutationsList) {
+                let temp = mutation.target.children[0].children[0];
+                debugger;
+                if (!mutation.target.hidden){
+                    temp.click();
+                    log('Click Ran');
+
+                }
+            }
+        };
+        var observer = new MutationObserver(callback);
+        observer.observe(targetNode, config);
+    }
+    function bootstrap(tries = 1) {
+        log('bootstrap attempt '+ tries);
+        if ($('#cont')) {
+            MO();
+            Enter();
+        } else if (tries < 1000) {
+            setTimeout(() => bootstrap(tries++), 200);
+        }
+    }
+    bootstrap();
+})();
