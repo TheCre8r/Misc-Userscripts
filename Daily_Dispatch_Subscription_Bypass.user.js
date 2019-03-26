c0fed7a47b8876ec56769fa5b79b018feea228df
Ian "The_Cre8r" Harshbarger <TheCre8r@users.noreply.github.com>
2019-03-26 10:17:05 -0400

    Update Daily_Dispatch_Subscription_Bypass.user.js

--- Daily_Dispatch_Subscription_Bypass.user.js
+++ Daily_Dispatch_Subscription_Bypass.user.js
@@ -1,17 +1,19 @@
 // ==UserScript==
 // @name         Daily Dispatch Subscription Bypass
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  try to take over the world!
 // @author       Ian
 // @require      http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js
 // @include      http://www.hendersondispatch.com/*
+// @include      https://hendersondispatch.com/
 // @grant        none
 // ==/UserScript==
 
 (function() {
     'use strict';
-
+    $('.body').show()
+    $('.summary').hide()
     $('.subscription-required').remove();
     $('.subscriber-only').removeClass('hide');
     $('.redacted-overlay').removeAttr('style');
